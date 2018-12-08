1e44ab86b05488d142ed17720349b982d0b0ad5d
Jan Soldát <soldier068@email.cz>
2018-12-08 18:47:39 +0100

    Added PUT, POST and DELETE

--- app.js
+++ app.js
@@ -10,4 +10,37 @@ const http = new easyHTTP;
   }
 }); */
 
+// Create data
+const data = {title: 'Data one',
+              body: 'Hovno'};
+
 // Make an post request
+/* http.setPost('https://jsonplaceholder.typicode.com/posts', data, function(err, suc) {
+  if (err) {
+    console.log('Chyba');
+    
+  } else {
+    console.log(suc);
+  }
+    
+})
+ */
+// Make an PUT request
+
+/* http.put('https://jsonplaceholder.typicode.com/posts/1', data, function(err, post) {
+  if (err) {
+    console.log('Chyba');
+    
+  } else {
+    console.log(post);
+  }
+}); */
+
+// DELETE Request
+ http.deletePost('https://jsonplaceholder.typicode.com/posts/1', function (posts, err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(posts);
+  }
+});
--- easyHTTP.js
+++ easyHTTP.js
@@ -19,5 +19,47 @@ class easyHTTP {
     this.http.send();
   }
 
+  // Create new post
+  setPost(url, data, callback) {
+    this.http.open('POST', url, true);
+    this.http.setRequestHeader('Content-type', 'application/json');
+
+    let self = this;
+    this.http.onload = function () {
+      callback(null, self.http.responseText)
+    }
+    this.http.send(JSON.stringify(data));
+  }
+
+  // PUT request
+  put(url, data, callback){
+    this.http.open('PUT', url, true);
+    this.http.setRequestHeader('Content-type', 'application/json');
+
+    let self = this;
+    this.http.onload = function () {
+      callback(null, self.http.responseText)
+    }
+    this.http.send(JSON.stringify(data));
+  }
+
+  deletePost(url, callback) {
+    this.http.open('DELETE', url, true);
+
+    // Přiřadit tenhle scoope do proměnný
+    let self = this;
+    this.http.onload = function () {
+      if (self.http.status === 200) {
+        callback('deleted', null);
+      } else {
+        callback(undefined, 'Chyba ' + self.http.status);
+      }
+    }
+
+    this.http.send();
+  }
+
 }
+
+
 
