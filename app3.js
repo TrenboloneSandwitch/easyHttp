0be4103c829f5f72d9c66868a99419aa59ead6f7
Bobby 6Killer <soldier068@email.cz>
2018-12-12 21:57:02 +0100

    Added version 3 -- async, await

---
+++ app3.js
@@ -0,0 +1,28 @@
+const http = new easyHTTPv2;
+// GET
+/*  http.get('https://jsonplaceholder.typicode.com/users')
+.then(data => {
+  data.forEach((name) => console.log(name.name));
+})
+.catch(err => console.log(err));
+ */
+// USer data
+const data = {
+  name: 'John',
+  username: 'johnjohn',
+  email: 'jdoe@sss.cz'
+}
+// Create Post
+/* http.post('https://jsonplaceholder.typicode.com/users', data)
+  .then(data => console.log(data))
+  .catch(err => console.log(err)); */
+
+/* // Update Post
+http.put('https://jsonplaceholder.typicode.com/users/2', data)
+  .then(data => console.log(data))
+  .catch(err => console.log(err)); */
+
+  // DElete Post
+http.delete('https://jsonplaceholder.typicode.com/users/2')
+.then(data => console.log(data))
+.catch(err => console.log(err));
